88d0169b917dacbb089d2fe5a87feb65c2b0cb08
ujjwal-2504 <code.with.ujjwal.25@gmail.com>
2024-09-10 07:12:05 +0530

    Completed Array

--- Javascript/arrays.js
+++ Javascript/arrays.js
@@ -1,3 +1,19 @@
-let students = ["Ujjwal", "Pushkar", "Aman", "Sharadha", "Mohini"];
+let students = ["Ujjwal", "Pushkar", "Aman", "Sharadha", "Alia"];
 
 let info = ["Ujjwal", 1, 99.9];
+
+let cars = ["Audi", "BMW", "TATA", "Hundai", "Lambo", "Ferrai", "XUV", "Toyota"];
+
+let months = ["January", "July", "March", "August"];
+
+let primary = ["red", "yellow", "blue"];
+
+let secondary = ["orange", "green", "violet"];
+
+const prices = [10, 12, 14, 16, 18, 20];
+
+let nums = [[2, 4], [6, 8], [10, 12], [14, 16], [18, 20]]; 
+
+let names = [["Ujjwal", "Pushkar"], ["Aman", "Raman"], ["Sharadha", "Bhakti"]];
+
+let tickTacToe = [['X', null, 'O'], [null, 'X', null], ['O', null, 'X']];
