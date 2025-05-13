1b2bd2a5621f851bd9c1d1dcce88dd9154ad7f6d
ujjwal-2504 <code.with.ujjwal.25@gmail.com>
2025-05-13 21:17:10 +0530

    completed Currency Converter Project

--- React/Chai_Aur_React/Projects/src/App.jsx
+++ React/Chai_Aur_React/Projects/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import BgColorChanger from "./Projects/BgColorChanger";
 import PasswordGenerator from "./Projects/PasswordGenerator";
+import CurrencyConverter from "./Projects/CurrencyConverter";
 
 function App() {
   return (
     <>
-      <PasswordGenerator />
+      <CurrencyConverter />
     </>
   );
 }
